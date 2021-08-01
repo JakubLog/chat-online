2c114050b7e50e5fa75e85a866e370385c8c4e7e
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-01 14:55:28 +0200

    Add useAuth hook

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,41 @@
+import firebase, { auth } from '../firebase';
+import React, { useEffect, useState, useContext } from 'react';
+
+const AuthContext = React.createContext({});
+const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setLoadingState] = useState(true);
+
+  const signIn = (login, password) => auth.signInWithEmailAndPassword(login, password);
+  const signOut = () => auth.signOut();
+  const signInWithGoogle = () => {
+    const provider = firebase.auth.GoogleAuthProvider();
+    return auth.signInWithPopup(provider);
+  };
+
+  useEffect(() => {
+    setLoadingState(true);
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+    setLoadingState(false);
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signIn,
+    signInWithGoogle,
+    signOut
+  };
+  return <AuthContext.Provider value={value}>{isLoading ? <p>Loading...</p> : children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+  const auth = useContext(AuthContext);
+  if (!auth) return console.error('Ooops! Something went wrong!');
+  return auth;
+};
+
+export default AuthProvider;
