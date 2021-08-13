bde0747f3d86aa04b7a8e1b2282e1f73dcaa236d
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-13 04:50:20 +0200

    Add prop-types & default message for error block

--- src/components/molecules/ErrorBlock/ErrorBlock.js
+++ src/components/molecules/ErrorBlock/ErrorBlock.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Wrapper, Title, Content } from './ErrorBlock.styles';
 
-const ErrorBlock = ({ message }) => {
+const defaultMessage = 'Sorry, something went wrong. Please try again or contact with support!';
+
+const ErrorBlock = ({ message = defaultMessage }) => {
   return (
     <Wrapper>
       <Title>Ooops!</Title>
       <Content>{message}</Content>
     </Wrapper>
   );
+};
+
+ErrorBlock.propTypes = {
+  message: PropTypes.string
 };
 
 export default ErrorBlock;
